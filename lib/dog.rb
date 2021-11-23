b595835f165c7c94ee3e1113c3be747a641db5e6
Luis Escobar <lae311@stern.nyu.edu>
2021-11-23 11:23:10 -0500

    commit tree f3cb7e1d86dc585171970ff3547dfd51d70b3be0

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,26 @@
 class Dog
+    # when we initialize it is best to set a parameter
+    def breed(name)
+        @name=name
+    end
+        # setter acts as an updater
+    def name=(name)
+        @name = name
+    end
+    #  getter acts as the accessor
+    def name
+        @name
+    end
 
+    def breed(breed)
+        @breed = breed
+    end
+
+    def breed= breed
+        @breed = breed
+    end
+
+    def breed
+        @breed
+    end
 end
